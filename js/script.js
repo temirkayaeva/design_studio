42c384c886b81ddc73fcd28bd73cb0176f822923
temirkayaeva <arianasanders62@gmail.com>
2021-07-21 17:24:41 +0300

    last section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-var a = document.querySelector(".little_text")
+const left_arrow_in_web_avatars = document.querySelector(".web_avatars .button_left")
+const right_arrow_in_web_avatars = document.querySelector(".web_avatars .right_left")
+const picture_in_web = document.querySelector(".web_avatars .picture")
 
-a.addEventListener("click", function() {
-  console.log(a);
-  a.classList.add("price");
+left_arrow_in_web_avatars.addEventListener("click", function() {
+  picture_in_web.classList.remove("image")
+  picture_in_web.classList.add("click_on_left_arrow")
 })
