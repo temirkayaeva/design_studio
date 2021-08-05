ab3299595317599e8fe8e14dcbfd34bf8e91d559
temirkayaeva <40575841+temirkayaeva@users.noreply.github.com>
2021-08-05 12:59:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,32 +13,6 @@ right_arrow_in_web_avatars.addEventListener("click", () => {
   picture_in_web.classList.add("click_on_right_arrow")
 });
 
-// link1 = document.querySelector('.link1');
-// link2 = document.querySelector('.link2');
-// link3 = document.querySelector('.link3');
-// link4 = document.querySelector('.link4');
-
-// link1.addEventListener('mouseover', underlineOn);
-// link1.addEventListener('mouseout', underlineOff);
-// link2.addEventListener('mouseover', underlineOn);
-// link2.addEventListener('mouseout', underlineOff);
-// link3.addEventListener('mouseover', underlineOn);
-// link3.addEventListener('mouseout', underlineOff);
-// link4.addEventListener('mouseover', underlineOn);
-// link4.addEventListener('mouseout', underlineOff);
-
-// function underlineOn () {
-//   line = this.querySelector('div');
-//   line.classList.toggle('underline');
-// }
-
-// function underlineOff() {
-//   line = this.querySelector('div');
-//   line.classList.remove('underline');
-// }
-
-// allLinks = document.querySelectorAll('.link');
-
 
 
 allLinks = document.querySelectorAll('.link a');
