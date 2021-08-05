bdd0d063e37915b4e86ab06f8b313af0f4bf0fe2
temirkayaeva <arianasanders62@gmail.com>
2021-08-05 12:54:10 +0300

    nav using js

--- js/script.js
+++ js/script.js
@@ -13,22 +13,49 @@ right_arrow_in_web_avatars.addEventListener("click", () => {
   picture_in_web.classList.add("click_on_right_arrow")
 });
 
+// link1 = document.querySelector('.link1');
+// link2 = document.querySelector('.link2');
+// link3 = document.querySelector('.link3');
+// link4 = document.querySelector('.link4');
 
-const allLinks = document.querySelectorAll('.link');
+// link1.addEventListener('mouseover', underlineOn);
+// link1.addEventListener('mouseout', underlineOff);
+// link2.addEventListener('mouseover', underlineOn);
+// link2.addEventListener('mouseout', underlineOff);
+// link3.addEventListener('mouseover', underlineOn);
+// link3.addEventListener('mouseout', underlineOff);
+// link4.addEventListener('mouseover', underlineOn);
+// link4.addEventListener('mouseout', underlineOff);
 
-console.log(allLinks);
+// function underlineOn () {
+//   line = this.querySelector('div');
+//   line.classList.toggle('underline');
+// }
 
-for (i = 0; i < allLinks.length; i++) {
-    allLinks[i].onmouseover = function() {
-      allUnderlines = document.querySelectorAll('.link div');
-      for (j = 0; j < allUnderlines.length; j++) {  
-        allUnderlines[j].classList.add('underline');
-      }
-    }
-    allLinks[i].onmouseout = function() {
-      allUnderlines = document.querySelectorAll('.link div');
-      for (j = 0; j < allUnderlines.length; j++) {  
-        allUnderlines[j].classList.remove('underline');
-      }
-    }
+// function underlineOff() {
+//   line = this.querySelector('div');
+//   line.classList.remove('underline');
+// }
+
+// allLinks = document.querySelectorAll('.link');
+
+
+
+allLinks = document.querySelectorAll('.link a');
+
+for (let item of allLinks) {
+  item.addEventListener('mouseover', function (e) {
+    console.log(this);
+    console.log(e);
+    let underline = e.target.children;
+    console.log(underline);
+    underline[0].classList.add('underline');
+  });
+  item.addEventListener('mouseout', function (e) {
+    console.log(this);
+    console.log(e);
+    let underline = e.target.children;
+    console.log(underline);
+    underline[0].classList.remove('underline');
+  });
 }
